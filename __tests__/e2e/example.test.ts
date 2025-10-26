beea8367fb215dcb7ac5935ea25d7d3d3ef86a58
ixuz <ixuz07@gmail.com>
2025-10-26 02:31:06 +0200

    test: add e2e tests using playwright

---
+++ __tests__/e2e/example.test.ts
@@ -0,0 +1,26 @@
+import { test, expect } from "@playwright/test";
+
+test("has title", async ({ page }) => {
+  await page.goto("/");
+  await expect(page).toHaveTitle(/^Woori$/);
+});
+
+test("has welcome text", async ({ page }) => {
+  await page.goto("/");
+  const content = await page.content();
+  expect(content).toContain(
+    'Välkommen till Woori, en restaurang med koreanska smaker. Namnet "Woori" betyder "vi" på koreanska, och här är alla en del av vår gemenskap. Njut av våra koreanska rätter!'
+  );
+});
+
+test("has restaurant street address", async ({ page }) => {
+  await page.goto("/");
+  const content = await page.content();
+  expect(content).toContain("Stagneliusvägen 47, 112 57, Stockholm");
+});
+
+test("has contact email", async ({ page }) => {
+  await page.goto("/");
+  const content = await page.content();
+  expect(content).toContain("kontakt@wooristockholm.se");
+});
