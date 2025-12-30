adfcc34c63ed0e7c7f1ca03673f26c7a5cd946ea
Anton Wiklund <ixuz07@gmail.com>
2025-12-30 09:39:13 +0100

    Revert "refactor: map available locales in generateStaticParams"
    This reverts commit cf430a80f204fe786fb61515e25ff2c15f03c9cb.

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,10 +1,9 @@
 import Home from "@/components/Home";
-import messages from "@/locales/messages";
 
 export default function Page() {
   return <Home />;
 }
 
 export async function generateStaticParams() {
-  return Object.keys(messages).map(key => ({ locale: key }))
+  return [{ locale: "sv" }, { locale: "en" }, { locale: "ko" }];
 }
