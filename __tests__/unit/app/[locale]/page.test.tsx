a08af557c541a2083eca133aef89b758cbd04c28
Anton Wiklund <ixuz07@gmail.com>
2025-12-30 09:33:50 +0100

    refactor: move generateStaticParams to layout file

--- __tests__/unit/app/[locale]/page.test.tsx
+++ __tests__/unit/app/[locale]/page.test.tsx
@@ -20,18 +20,4 @@ describe("Page", () => {
     // Assert
     expect(container).toMatchSnapshot();
   });
-
-  it("exports generateStaticParams which lists of available locales", async () => {
-    // Arrange
-    const { generateStaticParams } = await vi.importActual<
-      // eslint-disable-next-line @typescript-eslint/consistent-type-imports
-      typeof import("@/app/[locale]/page")
-    >("@/app/[locale]/page");
-
-    // Act
-    const staticParams = await generateStaticParams();
-
-    // Assert
-    expect(staticParams).toMatchSnapshot();
-  });
 });
--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,10 +1,5 @@
 import Home from "@/components/Home";
-import messages from "@/locales/messages";
 
 export default function Page() {
   return <Home />;
-}
-
-export async function generateStaticParams() {
-  return Object.keys(messages).map(key => ({ locale: key }))
 }
