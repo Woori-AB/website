0adfd56c64404a04644c25eda98d5b54945f425a
ixuz <ixuz07@gmail.com>
2026-01-03 09:00:26 +0100

    fix: avoid artifacts when rendering opening hours HTML element

--- src/components/OpeningHours.tsx
+++ src/components/OpeningHours.tsx
@@ -10,17 +10,72 @@ export default function OpeningHours() {
       <h1>{intl.formatMessage({ id: "opening-hours" })}</h1>
       <h2>{intl.formatMessage({ id: "opening-hours-subtitle" })}</h2>
       <span>
-        {intl.formatMessage({ id: "monday" })}:{" "}
-        {intl.formatMessage({ id: "closed" })}
+        {intl.formatMessage(
+          { id: "opening-hours-close" },
+          {
+            day: intl.formatMessage({ id: "monday" }),
+            close: intl.formatMessage({ id: "closed" }),
+          }
+        )}
       </span>
-      <span>{intl.formatMessage({ id: "tuesday" })}: 11:00 - 20:00</span>
-      <span>{intl.formatMessage({ id: "wednesday" })}: 11:00 - 20:00</span>
-      <span>{intl.formatMessage({ id: "thursday" })}: 11:00 - 20:00</span>
-      <span>{intl.formatMessage({ id: "friday" })}: 11:00 - 20:00</span>
-      <span>{intl.formatMessage({ id: "saturday" })}: 11:00 - 20:00</span>
       <span>
-        {intl.formatMessage({ id: "sunday" })}:{" "}
-        {intl.formatMessage({ id: "closed" })}
+        {intl.formatMessage(
+          { id: "opening-hours-open" },
+          {
+            day: intl.formatMessage({ id: "tuesday" }),
+            open: "11:00",
+            close: "20:00",
+          }
+        )}
+      </span>
+      <span>
+        {intl.formatMessage(
+          { id: "opening-hours-open" },
+          {
+            day: intl.formatMessage({ id: "wednesday" }),
+            open: "11:00",
+            close: "20:00",
+          }
+        )}
+      </span>
+      <span>
+        {intl.formatMessage(
+          { id: "opening-hours-open" },
+          {
+            day: intl.formatMessage({ id: "thursday" }),
+            open: "11:00",
+            close: "20:00",
+          }
+        )}
+      </span>
+      <span>
+        {intl.formatMessage(
+          { id: "opening-hours-open" },
+          {
+            day: intl.formatMessage({ id: "friday" }),
+            open: "11:00",
+            close: "20:00",
+          }
+        )}
+      </span>
+      <span>
+        {intl.formatMessage(
+          { id: "opening-hours-open" },
+          {
+            day: intl.formatMessage({ id: "saturday" }),
+            open: "11:00",
+            close: "20:00",
+          }
+        )}
+      </span>
+      <span>
+        {intl.formatMessage(
+          { id: "opening-hours-close" },
+          {
+            day: intl.formatMessage({ id: "sunday" }),
+            close: intl.formatMessage({ id: "closed" }),
+          }
+        )}
       </span>
     </div>
   );
